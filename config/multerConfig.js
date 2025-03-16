2bf70e044297c59265d1e584eed98dc0cb88f3e6
Sulthanzuza <sulthanpriv@gmail.com>
2025-03-16 16:38:31 +0530

    commited changes on multer

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,22 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const uploadDir = process.env.UPLOAD_DIR || path.join(__dirname, 'uploads');
+
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const multer = require('multer');
 
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-
-    cb(null, process.env.UPLOAD_DIR || 'uploads/');  
+    cb(null, uploadDir);  
   },
-
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);  
   },
-
 });
 
-
-
 const upload = multer({ storage: storage });
-
 
 module.exports = upload;
